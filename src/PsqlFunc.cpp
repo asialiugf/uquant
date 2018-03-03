2c8830f4487e69ea1e9f8a36ae0b948faad8e56a
asialiugf <asialiugf@sina.com>
2018-03-04 00:14:49 +0800

    .

--- src/PsqlFunc.cpp
+++ src/PsqlFunc.cpp
@@ -10,6 +10,7 @@
 namespace uBEE
 {
 
+
 void createBarTable(std::shared_ptr<uBEE::DBPool> dbpool,const char * future, const char * period)
 {
   auto conn = dbpool->getDBConn();
@@ -20,9 +21,20 @@ void createBarTable(std::shared_ptr<uBEE::DBPool> dbpool,const char * future, co
   char       ca_state[4096];
   memset(ca_state,'\0',4096);
 
-  const unsigned char strSrc[]="\"";
+  const unsigned char strSrc[]="\
+  datetime        VARCHAR(10) NOT NULL,\
+  serials         VARCHAR UNIQUE NOT NULL,\
+  high            DOUBLE PRECISION NOT NULL,\
+  open            DOUBLE PRECISION NOT NULL,\
+  close		      DOUBLE PRECISION NOT NULL,\
+  low		      DOUBLE PRECISION NOT NULL,\
+  open_oi	      DOUBLE PRECISION NOT NULL,\
+  close_oi        DOUBLE PRECISION NOT NULL,\
+  volume          DOUBLE PRECISION NOT NULL,\
+  PRIMARY KEY(datetime)\
+  ";
 
-  sprintf(ca_state,"CREATE TABLE BAR_%s(%s); ",future,strSrc);
+  sprintf(ca_state,"CREATE TABLE BAR_%s_%s(%s); ",future,period,strSrc);
   std::cout << ca_state << std::endl;
 
   res = PQexec(connx, ca_state);
